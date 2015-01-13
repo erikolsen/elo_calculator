f75141be95d86bf4d745fe0ad5017aac55e74ef3
Erik Olsen <erik.s.olsen@gmail.com>
2015-01-13 12:39:07 -0600

    Game creator saves the rating you had during play

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :players, only: [:index, :show, :create, :new]
-  resources :games, only: [:new, :create]
+  resources :games, only: [:index, :new, :create]
 
   root 'players#index'
 end
