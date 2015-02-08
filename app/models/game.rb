26e0880886f99b9b7184f07c3e5d09511c411cd3
Erik Olsen <erik.s.olsen@gmail.com>
2015-02-08 00:18:47 -0600

    Only show the undo button if game can be rolled back

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,9 @@ class Game < ActiveRecord::Base
   belongs_to :loser, class_name: 'Player'
 
   validates_presence_of :winner_id, :loser_id, :winner_rating, :loser_rating
+
+  def can_undo?
+    self == Game.last
+  end
 end
 
