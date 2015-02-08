46c0a943e021c02edb987259b4b328bd7ab905b7
Erik Olsen <erik.s.olsen@gmail.com>
2015-02-07 21:26:25 -0600

    Try to get foundation icons to precompile

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module EloCalculator
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Central Time (US & Canada)'
     config.serve_static_assets = true
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
