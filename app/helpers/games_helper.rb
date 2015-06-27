7dc1f3f7abaa2886688a071631fddb24f66113f4
Erik Olsen <erik.s.olsen@gmail.com>
2015-06-27 14:16:42 -0500

    Default to show last winner when adding game

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,4 +2,8 @@ module GamesHelper
   def all_players
     Player.by_name
   end
+
+  def all_players_last_winner_default
+    Game.last ? all_players.unshift(Game.last.winner) : all_players
+  end
 end
