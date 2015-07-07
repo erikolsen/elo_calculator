aa884a38bad08a1e11a47bf06ae5a6743983491a
Erik Olsen <erik.s.olsen@gmail.com>
2015-07-06 21:43:45 -0500

    Use optimized list until add login with facebook

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,10 +4,7 @@ module GamesHelper
   end
 
   def all_players_last_winner_default
-    #optimized_player_list = all_players.unshift(Game.last.winner).push(Game.last.loser)
-    last_winner = Player.where(id: Game.last.winner.id)
-    last_loser = Player.where(id: Game.last.loser.id)
-    optimized_player_list = last_winner + all_players + last_loser
+    optimized_player_list = all_players.unshift(Game.last.winner).push(Game.last.loser)
     Game.last ? optimized_player_list : all_players
   end
 end
