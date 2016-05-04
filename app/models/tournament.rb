cdb4de4d619331d85b2b46162e3053b0acad45c3
Erik Olsen <erik.s.olsen@gmail.com>
2016-05-04 17:44:21 -0500

    Show results on tournament page

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,4 +2,8 @@ class Tournament < ActiveRecord::Base
   has_many :entries
   has_many :players, through: :entries
   has_many :matchups
+
+  def match_points_for(player)
+    matchups.where(winner: player).count
+  end
 end
