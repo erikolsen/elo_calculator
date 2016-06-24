4456138059a666b28642842c34a2d2d5d87fc19f
Erik Olsen <erik.s.olsen@gmail.com>
2016-06-23 21:02:22 -0500

    Homepage lets you create club

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   resources :games, only: [:index, :new, :create, :show, :destroy]
   resources :matchups, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :tournaments, only: [:index, :new, :update, :create, :show]
-  resources :clubs, only: :show, param: :slug
   resources :clubs, only: [:new, :create] do
     scope module: :clubs do
       resources :memberships, only: [:new, :create]
     end
   end
+  resources :clubs, only: :show, param: :slug
 
   root 'clubs#index'
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,27 +1,44 @@
 require 'rails_helper'
 
-describe 'players page' do
-  let!(:player1) { Player.create! name: 'player 1', rating: 500 }
-  let!(:player2) { Player.create! name: 'player 2', rating: 600 }
+describe 'homepage' do
+  describe 'starting a club' do
+    let(:club_name) { Faker::Team.name }
 
-  let!(:game1) { Game.create! winner_id: player1.id, loser_id: player2.id, winner_rating: 400, loser_rating: 500 }
-  let!(:game2) { Game.create! winner_id: player2.id, loser_id: player1.id, winner_rating: 500, loser_rating: 400 }
+    it 'adds the club link to the homepage' do
+      visit root_path
+      click_link 'Start a Club'
+      fill_in :club_name, with: club_name
+      click_button 'Add Club'
 
-  it 'shows player ratings' do
-    visit players_path
-
-    within "#player-#{player1.id}" do
-      expect(find('.rank')).to have_content(2)
-      expect(find('.name')).to have_content(player1.name)
-      expect(find('.rating')).to have_content(player1.rating)
-      expect(find('.won-lost')).to have_content("1 / 1")
+      expect(page).to have_content(club_name)
+      expect(page).to have_content('Start a Club')
     end
+  end
 
-    within "#player-#{player2.id}" do
-      expect(find('.rank')).to have_content(1)
-      expect(find('.name')).to have_content(player2.name)
-      expect(find('.rating')).to have_content(player2.rating)
-      expect(find('.won-lost')).to have_content("1 / 1")
+  describe 'has access to the all players page' do
+    let!(:player1) { Player.create! name: 'player 1', rating: 500 }
+    let!(:player2) { Player.create! name: 'player 2', rating: 600 }
+
+    let!(:game1) { Game.create! winner_id: player1.id, loser_id: player2.id, winner_rating: 400, loser_rating: 500 }
+    let!(:game2) { Game.create! winner_id: player2.id, loser_id: player1.id, winner_rating: 500, loser_rating: 400 }
+
+    it 'shows player ratings' do
+      visit root_path
+      click_link 'All Players'
+
+      within "#player-#{player1.id}" do
+        expect(find('.rank')).to have_content(2)
+        expect(find('.name')).to have_content(player1.name)
+        expect(find('.rating')).to have_content(player1.rating)
+        expect(find('.won-lost')).to have_content("1 / 1")
+      end
+
+      within "#player-#{player2.id}" do
+        expect(find('.rank')).to have_content(1)
+        expect(find('.name')).to have_content(player2.name)
+        expect(find('.rating')).to have_content(player2.rating)
+        expect(find('.won-lost')).to have_content("1 / 1")
+      end
     end
   end
 
