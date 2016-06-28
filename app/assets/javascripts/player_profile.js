3b05c3b755d5c4df2a3360516044e2db7b0b1795
Erik Olsen <erik.s.olsen@gmail.com>
2016-06-27 19:28:10 -0500

    Removes hight on tabs. Adds columns to profile info.

--- app/assets/javascripts/player_profile.js
+++ app/assets/javascripts/player_profile.js
@@ -1,6 +1,5 @@
 $(document).on('page:change', function () {
   $("#tabs").tabs({
-    heightStyle: 'fill',
     active: localStorage.getItem("currentTabIndex"),
     activate: function(event, ui) {
       localStorage.setItem("currentTabIndex", ui.newTab[0].dataset["tabIndex"]);
