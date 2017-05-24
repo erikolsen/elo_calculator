60aa44ed0f5dab87db4e91c6003857e13d996836
Erik Olsen <erik.s.olsen@gmail.com>
2017-05-24 17:18:43 -0500

    Use new expanded class for buttons

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -6,7 +6,7 @@ module PlayersHelper
     loser_wins = games_played.where(winner_id: opponent.id).count
     percent = (player_wins.to_f / games_played.count.to_f).round(2) * 100.0
     link_title = "#{player.name} vs. #{opponent.name}"
-    link_to matchups_path(matchup: { primary_id: player.id, secondary_id: opponent.id}), method: 'post', class: 'button expand rematchLink' do
+    link_to matchups_path(matchup: { primary_id: player.id, secondary_id: opponent.id}), method: 'post', class: 'button expanded rematchLink' do
       link = content_tag(:span, number_to_percentage(percent, precision: 0), class: 'left')
       link += content_tag(:span, link_title, class: 'center')
       link += content_tag(:span, "#{player_wins} / #{loser_wins}", class: 'right')
