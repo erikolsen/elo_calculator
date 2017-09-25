2f24ca12b91867c8267bb3deb39d996442fd2db5
Erik Olsen <erik.s.olsen@gmail.com>
2017-09-24 21:02:41 -0500

    Can only add player to Round Robin

--- app/controllers/round_robins_controller.rb
+++ app/controllers/round_robins_controller.rb
@@ -3,4 +3,22 @@ class RoundRobinsController < ApplicationController
     @tournament = RoundRobin.find(params[:id])
     @player = Player.find(params[:player]) if params[:player]
   end
+
+  def update
+    @tournament = RoundRobin.find(params[:id])
+    if round_robin_params[:players].present?
+      new_player = Player.find round_robin_params[:players]
+      @tournament.add_player new_player
+      redirect_to @tournament
+    else
+      flash.now[:alert] = 'Failed to add player'
+      flash.keep
+      redirect_to @tournament
+    end
+  end
+  private
+
+  def round_robin_params
+    params.require(:round_robin).permit({ players: [] }, :players)
+  end
 end
