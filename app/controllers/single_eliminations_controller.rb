aab9ea1a29b7c4dba44c5fe3ccb07d5b3a4cb0e3
Erik Olsen <erik.s.olsen@gmail.com>
2017-10-08 17:56:05 -0500

    Don't set player for single elimination show page

--- app/controllers/single_eliminations_controller.rb
+++ app/controllers/single_eliminations_controller.rb
@@ -1,7 +1,6 @@
 class SingleEliminationsController < ApplicationController
   def show
     @tournament = SingleElimination.find(params[:id])
-    @player = Player.find(params[:player]) if params[:player]
     redirect_to tournament_entries_path(@tournament) unless @tournament.started?
   end
 end
