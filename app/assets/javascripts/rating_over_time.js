57a1cd1a72e1ca6c0a104b6c17cbb06570574ffb
Erik Olsen <erik.s.olsen@gmail.com>
2018-02-04 15:09:03 -0600

    Fix Rating Over Time to work with turbolinks

--- app/assets/javascripts/rating_over_time.js
+++ app/assets/javascripts/rating_over_time.js
@@ -1,62 +1,64 @@
-$(function() {
-  var playerId = $('#rating-over-time-graph').data('player-id');
-  var limit = $('#rating-over-time-graph').data('limit');
-  if(playerId) {
-    $.getJSON('/player_stats/' + playerId+'?limit='+limit)
-      .error(function() {
-        console.log('could not get player stats for: ' + playerId);
-      })
-    .done(function(data) {
-      var ctx = $('#rating-over-time-graph');
-      var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: xAxisData(data),
-          datasets: [
-          {
-            label: 'Rating Over Time',
-            lineTension: 0,
-            data: yAxisData(data),
-            backgroundColor: [
-              'rgba(0, 153, 255, 0.2)'
-            ],
-            borderColor: [
-              'rgba(0, 153, 255, 1)'
-            ],
-            borderWidth: 1
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    var playerId = $('#rating-over-time-graph').data('player-id');
+    var limit = $('#rating-over-time-graph').data('limit');
+    if(playerId) {
+      $.getJSON('/player_stats/' + playerId+'?limit='+limit)
+        .error(function() {
+          console.log('could not get player stats for: ' + playerId);
+        })
+      .done(function(data) {
+        var ctx = $('#rating-over-time-graph');
+        var chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: xAxisData(data),
+            datasets: [
+            {
+              label: 'Rating Over Time',
+              lineTension: 0,
+              data: yAxisData(data),
+              backgroundColor: [
+                'rgba(0, 153, 255, 0.2)'
+              ],
+              borderColor: [
+                'rgba(0, 153, 255, 1)'
+              ],
+              borderWidth: 1
+            }
+            ]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+              xAxis: [{
+                type: 'time',
+                time: {
+                  unit: 'll'
+                }
+              }]
+            }
           }
-          ]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: true,
-          scales: {
-            xAxis: [{
-              type: 'time',
-              time: {
-                unit: 'll'
-              }
-            }]
-          }
-        }
+        });
       });
-    });
 
-    function formatDate(s) {
-      var date = new Date(s);
-      return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
-    }
+      function formatDate(s) {
+        var date = new Date(s);
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
+      }
 
-    function xAxisData(data) {
-      return $.map(data, function(datum) {
-        return formatDate(datum.x);
-      });
-    }
+      function xAxisData(data) {
+        return $.map(data, function(datum) {
+          return formatDate(datum.x);
+        });
+      }
 
-    function yAxisData(data) {
-      return $.map(data, function(datum) {
-        return datum.y;
-      });
+      function yAxisData(data) {
+        return $.map(data, function(datum) {
+          return datum.y;
+        });
+      }
     }
-  }
+  });
 });
