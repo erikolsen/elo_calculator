c267f6b3b53bf4d909ceb426d8bce0bb9522f9fe
Erik Olsen <erikolsen@Eriks-Laptop.local>
2023-08-20 17:01:35 -0500

    do not precompile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module EloCalculator
     config.load_defaults 5.2
     config.time_zone = 'Central Time (US & Canada)'
     config.active_record.legacy_connection_handling = false
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
